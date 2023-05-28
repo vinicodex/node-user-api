fbd219d247ee5fb7f3aaeda0070041196e66afa1
vinicodex <mvnxr28@gmail.com>
2023-05-28 11:11:45 -0300

    config mongoose, database

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,27 +1,29 @@
+const asyncHandler = require("express-async-handler")
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContact = async (req, res) => {
+const getContact = asyncHandler ((req, res) => {
     res.status(200).json({"message":"Get all contacts"});
-};
+});
 
-const createContact = async (req, res) => {
+const createContact = asyncHandler ((req, res) => {
     const {name, email, phone} = req.body
     if(!name || email || phone) {
         res.status(400)
         throw new Error("All fields are mandatory");
     }
     res.status(201).json({"message":"Create contact"});
-};
+});
 
-const updateContact = async (req, res) => {
+const updateContact = asyncHandler ((req, res) => {
     res.status(200).json({"message":"Create contact"});
-};
+});
 
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler ((req, res) => {
     console.log(req.body)
     res.status(404).json({"message":`Delete contact for ${req.params.id}`});
-};
+});
 
 
 module.exports = {getContact, createContact, updateContact, deleteContact}
